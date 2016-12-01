637091d51576c427fe64829446a2dd34151f39e5
Thiago Andrade <thiagoh@gmail.com>
2016-11-30 21:46:09 -0500

    annotations to order

--- comp229-mvc-lesson-12/Models/Order.cs
+++ comp229-mvc-lesson-12/Models/Order.cs
@@ -3,7 +3,9 @@ namespace comp229_mvc_lesson_12.Models {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Web.Mvc;
     using System.Data.Entity.Spatial;
+    using System.ComponentModel;
 
     [Table("Order")]
     public partial class Order {
@@ -12,40 +14,59 @@ namespace comp229_mvc_lesson_12.Models {
             OrderDetails = new HashSet<OrderDetail>();
         }
 
+        [ScaffoldColumn(false)]
         public int OrderId { get; set; }
 
+        [ScaffoldColumn(false)]
         public DateTime OrderDate { get; set; }
 
+        [ScaffoldColumn(false)]
         [StringLength(256)]
         public string Username { get; set; }
 
+        [Required(ErrorMessage = "First name is required")]
         [StringLength(160)]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last name is required")]
         [StringLength(160)]
+        [DisplayName("Last name")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Address is required")]
         [StringLength(70)]
         public string Address { get; set; }
 
+        [Required(ErrorMessage = "City is required")]
         [StringLength(40)]
         public string City { get; set; }
 
+        [Required(ErrorMessage = "State is required")]
         [StringLength(40)]
         public string State { get; set; }
 
+        [Required(ErrorMessage = "Postal code is required")]
         [StringLength(10)]
         public string PostalCode { get; set; }
 
+        [Required(ErrorMessage = "Country is required")]
         [StringLength(40)]
         public string Country { get; set; }
 
+        [Required(ErrorMessage = "Phone numbrer is required")]
         [StringLength(24)]
         public string Phone { get; set; }
 
-        [StringLength(160)]
+        [Required(ErrorMessage = "Email address is required")]
+        [DisplayName("Email address")]
+
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+            ErrorMessage = "Email is is not valid.")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [ScaffoldColumn(false)]
         [Column(TypeName = "numeric")]
         public decimal Total { get; set; }
 
