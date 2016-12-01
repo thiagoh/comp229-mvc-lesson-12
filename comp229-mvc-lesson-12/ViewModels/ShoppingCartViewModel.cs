9f93d9ac26f7ff8163099ceaa73c03e37066165a
Thiago Andrade <thiagoh@gmail.com>
2016-11-30 22:16:15 -0500

    shopping cart

---
+++ comp229-mvc-lesson-12/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using comp229_mvc_lesson_12.Models;
+
+namespace comp229_mvc_lesson_12.ViewModels {
+    public class ShoppingCartViewModel {
+
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
